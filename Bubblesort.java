de1aedc2c7f37c3b67d7260bf96d6af0a459f79b
Felix Rabe <public@felixrabe.net>
2011-10-24 18:41:17 +0200

    Implement main; javadoc

--- Bubblesort.java
+++ Bubblesort.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 /** Program that sorts an arbitrary array of integers using the bubble sort algorithm.
- *
  * @author Felix Rabe
  * @version 20111024
  */
@@ -9,8 +8,40 @@ public class Bubblesort {
     public static final Random random = new Random();
     public static final int TEST_DATA_LENGTH = 20;
 
+    public static void main(String[] args) {
+        int[] inputs;
+        if (args.length == 0) {
+            inputs = generateRandomIntArray();
+        } else {
+            inputs = stringsToInts(args);
+        }
+        int[] outputs = inputs.clone();
+        sortInPlace(outputs);
+        printOut(inputs, outputs);
+    }
+
+    private static int[] stringsToInts(String[] strings) {
+        int[] ints = new int[strings.length];
+        for (int i = 0; i < ints.length; i++) {
+            String string = strings[i];
+            try {
+                ints[i] = Integer.parseInt(string);
+            } catch (NumberFormatException e) {
+                System.out.println("Is this a number? '" + string + "' - I don't think so!");
+                // I chose to keep invalid numbers as 0 in the array.
+            }
+        }
+        return ints;
+    }
+
+    private static void printOut(int[] inputs, int[] outputs) {
+        System.out.println("    Inputs:     Outputs:");
+        for (int i = 0; i < outputs.length; i++) {
+            System.out.printf("%12d %12d\n", inputs[i], outputs[i]);
+        }
+    }
+
     /** Generate a random array of integers.
-     *
      * @return the generated array
      */
     public static int[] generateRandomIntArray() {
@@ -18,7 +49,6 @@ public class Bubblesort {
     }
 
     /** Generate a random array of integers.
-     *
      * @param length the length of the array to be generated
      * @return the generated array
      */
@@ -31,7 +61,6 @@ public class Bubblesort {
     }
 
     /** Sort the specified integer array in-place.  Uses the bubble sort algorithm
-     *
      * @param array the array to sort
      */
     public static void sortInPlace(int[] array) {
@@ -48,6 +77,11 @@ public class Bubblesort {
         } while(hadToSwap);
     }
 
+    /** Swap two elements in the given array.
+     * @param array the array in which to swap the elements
+     * @param first the index of the first element to swap
+     * @param second the index of the second element to swap
+     */
     private static void swap(int[] array, int first, int second) {
         int tmp = array[first];
         array[first] = array[second];
